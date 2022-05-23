3298a532b0006c9700d1ae760ff69ad5704f25a4
austinhatch <32887233+austinhatch@users.noreply.github.com>
2022-05-22 22:41:27 -0400

    Create KeeperRegistryInterface.sol

---
+++ contracts/KeeperRegistryInterface.sol
@@ -0,0 +1,9 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+interface KeeperRegistryInterface {
+
+  function cancelUpkeep(uint256 id) external;
+
+  function addFunds(uint256 id, uint96 amount) external;
+}
